007f2f875200f008c6bbd6ab280fb786dcf55f66
Jan-Piet Mens <jp@mens.de>
2024-01-29 16:01:57 +0100

    Simplify password generation with own filter 	add support for pre-defined passwords in configuration.yaml

---
+++ files/ansible/filter_plugins/mkpasswords.py
@@ -0,0 +1,30 @@
+# mkpasswords.py, JPM, take list of users, add random
+# password if a dict has no 'password' element in it.
+
+import secrets
+import os
+
+def mkpasswords(users, directory):
+    newusers = []
+
+    print(directory)
+
+    for u in users:
+        if not "password" in u:
+            # if file exists, read its content (a.k.a password lookup)
+            p = os.path.join(directory, u["username"] + ".pass")
+            if os.path.exists(p):
+                with open(p, "r") as f:
+                    u["password"] = f.read()
+            else:
+                u["password"] = secrets.token_urlsafe(16)
+
+        newusers.append(u)
+
+    return newusers
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'mkpasswords': mkpasswords,
+        }
