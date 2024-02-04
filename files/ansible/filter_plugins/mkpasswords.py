6aec260615171865305d9e4466f0c0eec3c7ddf6
Jan-Piet Mens <jp@mens.de>
2024-02-04 11:42:43 +0000

    Add support for optional payload encryption 	closes #1

--- files/ansible/filter_plugins/mkpasswords.py
+++ files/ansible/filter_plugins/mkpasswords.py
@@ -1,6 +1,7 @@
 # mkpasswords.py, JPM, take list of users, add random
 # password if a dict has no 'password' element in it.
 
+from ansible.errors import AnsibleFilterError
 import secrets
 import os
 
@@ -20,6 +21,18 @@ def mkpasswords(users, directory):
                     u["password"] = f.read().rstrip()
             else:
                 u["password"] = secrets.token_urlsafe(16)
+
+        if "secret" in u:
+            ''' if the encryptionKey secret begins with a slash, it's
+                a file which contains the secret '''
+            if u["secret"].startswith("/"):
+                file = u["secret"]
+                if os.path.exists(file):
+                    with open(file, "r") as f:
+                        u["secret"] = f.read().rstrip()
+                else:
+                    raise AnsibleFilterError("mkpasswords: cannot open file {0} for user {1}".format(p, u["username"]))
+
 
         newusers.append(u)
 
